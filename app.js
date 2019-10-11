250fb48e85f69d457c1b1dc55942f3d80c5739ca
kls6378 <kls6378@naver.com>
2019-10-11 14:09:11 +0900

    Add Router

--- app.js
+++ app.js
@@ -3,12 +3,12 @@ const app = express()
 
 const port = 3000
 
+const mainRouter = require('./routes/main')
+
 app.set('view engine','pug')
 app.set('views','views')
 
-app.get('/', function (req, res) {
-  res.render('main',{name:'leesu'})
-})
+app.use('/',mainRouter)
 
 app.listen(3000, function () {
   console.log(`Listening on port ${port}!`)
