c5853fb97484e5f5bebcca59b09f6806bea150f7
kls6378 <kls6378@naver.com>
2019-12-23 14:25:33 +0900

    asdf

--- project/board/urls.py
+++ project/board/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='board'),
-    
+    path('create/', views.boardCreate, name='board_create'),
+    path('<int:board_id>/', views.boardDetail, name='board_detail'),
+    path('<int:board_id>/update/', views.boardUpdate, name='board_update'),
+    path('<int:board_id>/delete/', views.boardDelete, name='board_delete'),
 ]
--- project/board/views.py
+++ project/board/views.py
@@ -6,82 +6,93 @@ from board.models import Board, Comment
 def index(request):
     if not request.user.is_authenticated:
         return redirect('login')
+
     board_list=Board.objects.all().order_by('-date')
+
     return render(request, 'board/index.html', {'board_list':board_list})
 
+def boardCreate(request):
+    if not request.user.is_authenticated:
+        return redirect('login')
 
-# def detail(request, board_id):
-#     board=get_object_or_404(Board, pk=board_id)
+    if request.method == 'POST':
+        new_board = Board.objects.create(
+            title = request.POST['title'],
+            contents = request.POST['contents'],
+            author = request.user.username,
+            tag = request.POST['tag'],
+        )
+        return redirect(f'/board/{ new_board.id }')
 
-#     if request.method == 'POST':
-#         Comment.objects.create(
-#             board = board,
-#             name = request.POST['name'],
-#             password = request.POST['password'],
-#             contents = request.POST['contents']
-#         )
-#         return redirect(f'/board/{board_id}')
+    return render(request, 'board/create.html')
 
-#     return render(request, 'board/detail.html', {'board':board})
+def boardDetail(request, board_id):
+    if not request.user.is_authenticated:
+        return redirect('login')
+        
+    board = Board.objects.get(id=board_id)
 
+    if request.method == 'POST':
+        Comment.objects.create(
+            board = board,
+            author = request.user.username,
+            contents = request.POST['contents']
+        )
+        return redirect(f'/board/{board_id}')
 
-# def create(request):
-#     if request.method == 'POST':
-#         new_board = Board.objects.create(
-#             name = request.POST['name'],
-#             password = request.POST['password'],
-#             title = request.POST['title'],
-#             contents = request.POST['contents']
-#         )
-#         return redirect(f'/board/{ new_board.id }')
-
-#     return render(request, 'board/create.html')
-
-
-# def update(request, board_id):
-#     board = Board.objects.get(id=board_id)
-
-#     if request.method == 'POST':
-#         if request.POST['password'] == board.password:
-#             board.name = request.POST['name']
-#             board.title = request.POST['title']
-#             board.contents = request.POST['contents']
-#             board.save()
-#             return redirect(f'/board/{ board.id }')
-
-#     return render(request, 'board/update.html',{'board':board})
+    return render(request, 'board/detail.html', {'board':board})
 
 
-# def delete(request, board_id):
-#     board = Board.objects.get(id=board_id)
+def boardUpdate(request, board_id):
+    if not request.user.is_authenticated:
+        return redirect('login')
 
-#     if request.method == 'POST':
-#         if request.POST['password'] == board.password:
-#             board.delete()
-#             return redirect('/board/')
+    board = Board.objects.get(id=board_id)
 
-#     return render(request, 'board/delete.html',{'board':board})
+    if board.author != request.user.username:
+        redirect(f'/board/{ board_id }')
 
+    if request.method == 'POST':
+        board.title = request.POST['title']
+        board.contents = request.POST['contents']
+        board.tag = request.POST['tag']
+        board.save()
+        return redirect(f'/board/{ board.id }')
 
-# def cupdate(request, comment_id):
-#     comment = Comment.objects.get(id=comment_id)
-
-#     if request.method == 'POST':
-#         if request.POST['password'] == comment.password:
-#             comment.name = request.POST['name']
-#             comment.contents = request.POST['contents']
-#             comment.save()
-#             return redirect(f'/board/{ comment.board.id }')
-
-#     return render(request, 'board/cupdate.html',{'comment':comment})
+    return render(request, 'board/update.html',{'board':board})
 
 
-# def cdelete(request, comment_id):
-#     comment = Comment.objects.get(id=comment_id)
+def boardDelete(request, board_id):
+    board = Board.objects.get(id=board_id)
 
-#     if request.method == 'POST':
-#         if request.POST['password'] == comment.password:
-#             comment.delete()
-#             return redirect(f'/board/{comment.board.id}')
+    if request.method == 'POST':
+        if request.POST['password'] == board.password:
+            board.delete()
+            return redirect('/board/')
 
-#     return render(request, 'board/cdelete.html',{'comment':comment})
+    return render(request, 'board/delete.html',{'board':board})
+
+##################################################################################################################################
+
+def cupdate(request, comment_id):
+    comment = Comment.objects.get(id=comment_id)
+
+    if request.method == 'POST':
+        if request.POST['password'] == comment.password:
+            comment.name = request.POST['name']
+            comment.contents = request.POST['contents']
+            comment.save()
+            return redirect(f'/board/{ comment.board.id }')
+
+    return render(request, 'board/cupdate.html',{'comment':comment})
+
+
+def cdelete(request, comment_id):
+    comment = Comment.objects.get(id=comment_id)
+
+    if request.method == 'POST':
+        if request.POST['password'] == comment.password:
+            comment.delete()
+            return redirect(f'/board/{comment.board.id}')
+
+    return render(request, 'board/cdelete.html',{'comment':comment})
